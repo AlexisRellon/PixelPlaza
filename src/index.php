0be240de34fa41dc260e207be1d7319c957ca029
AlexisRellon <alexis26rellon@gmail.com>
2024-05-17 00:24:35 +0800

    chore: Remove unnecessary HTML elements and update CSS styles in index.php

--- src/index.php
+++ src/index.php
@@ -11,31 +11,17 @@
     <meta name="author" content="Pixel Plaza">
     <meta name="revisit-after" content="1 day">
     <meta name="language" content="English">
-    
+
     <link rel="stylesheet" href="../css/style.css" type="text/css" media="all" />
     <link rel="shortcut icon" href="../img/brand-logo-white.png" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Material+Icons+Outlined" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Material+Icons+Round" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet" />
-
-
+    
 </head>
+
 <body>
-    <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h1>
-    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
-    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
-    <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h5>
-    <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h6>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-    <blockquote>Lorem ipsum dolor sit amet consectetur adipisicing elit.</blockquote>
-    <pre>Lorem ipsum dolor sit amet consectetur adipisicing elit.</pre>
 
-    <br>
-
-    <span class="material-icons">home</span>
-    <span class="material-icons-outlined">home</span>
-    <span class="material-icons-round">home</span>
 </body>
 
 </html>
