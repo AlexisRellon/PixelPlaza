5c32a773f0b6507a2fff5ca200f843144a766eb9
AlexisRellon <alexis26rellon@gmail.com>
2024-05-18 00:02:31 +0800

    chore: Update index.php with new styles and elements, added navbar

--- src/index.php
+++ src/index.php
@@ -13,15 +13,52 @@
     <meta name="language" content="English">
 
     <link rel="stylesheet" href="../css/style.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="../css/global-style.css">
+    <link rel="stylesheet" href="../css/utility.min.css">
+
     <link rel="shortcut icon" href="../img/brand-logo-white.png" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Material+Icons+Outlined" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Material+Icons+Round" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet" />
-    
+
 </head>
 
 <body>
+    <div class="container-fluid p-5 flex justify-content-space-between align-items-center flex-wrap bg-black-500">
+        <div class="flex gap-15">
+            <a href="#" class="logo m-1">
+                <svg viewBox="0 0 511.57 491.49">
+                    <path class="fill-white" d="M440,35V317.6c0,2.65-1.05,5.2-2.93,7.07l-92.41,92.4c-1.88,1.88-4.42,2.93-7.07,2.93H205c-8.28,0-15-6.72-15-15v-70c0-8.28,6.72-15,15-15h120c8.28,0,15-6.72,15-15V135c0-8.28-6.72-15-15-15H155c-8.28,0-15,6.72-15,15V405c0,8.28-6.72,15-15,15H55c-8.28,0-15-6.72-15-15V35c0-8.28,6.72-15,15-15H425c8.28,0,15,6.72,15,15Z" />
+                    <rect class="fill-white" x="210" y="190" width="60" height="60" />
+                </svg>
+            </a>
 
+            <div class="nav-bar color-white flex align-items-center gap-15">
+                <a href="#">Home</a>
+                <a href="#">Sale</a>
+                <a href="#">Products</a>
+                <a href="#">Announcement</a>
+
+                <div class="h-100 flex align-items-center gap-5">
+                    <input type="search" name="search" class="search-bar bg-white-500 rounded-md color-black" placeholder="Search Products">
+                    <button class="search-btn" type="submit"><span class="material-icons">search</span></button>
+                </div>
+            </div>
+        </div>
+
+        <div class="h-100 flex align-items-center gap-5 color-white">
+            <a class="h-50" href="#">
+                <span class="material-icons-outlined">account_circle</span>
+            </a>
+            <a class="h-50" href="#">
+                <span class="material-icons-outlined">shopping_cart</span>
+            </a>
+        </div>
+    </div>
+
+    <div>
+
+    </div>
 </body>
 
 </html>
