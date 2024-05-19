8cbdde2e4a74d5d4ceed9bb9b8b2bd93fc0e13c0
AlexisRellon <alexis26rellon@gmail.com>
2024-05-20 01:01:44 +0800

    Update index.php with new styles and elements, added navbar

---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Pixel Plaza - Home</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Pixel Plaza is a e-commerce platform to buy the best computer peripherals in the market.">
+    <meta name="author" content="Pixel Plaza">
+    <meta name="revisit-after" content="1 day">
+    <meta name="language" content="English">
+
+    <link rel="stylesheet" href="../css/style.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="../css/global-style.css">
+    <link rel="stylesheet" href="../css/utility.min.css">
+
+    <link rel="shortcut icon" href="../img/brand-logo-white.png" type="image/x-icon">
+    <link rel="icon" href="../img/brand-logo-white.png" type="image/x-icon">
+
+    <script src="../js/script.js"></script>
+
+
+</head>
+
+<body>
+
+    <!-- <div class="cookie-banner color-black flex justify-center align-items-center" id="cookie-banner">
+        <p class="m-0">We use cookies to improve your experience on our website. By browsing this website, you agree to our use of cookies.</p>
+        <input type="button" class="btn m-3 p-1 bg-black-500 color-white rounded-md" onclick="acceptCookies()" value="Accept">
+    </div> -->
+
+    <a type="button" class="to-top absolute rounded-full invisible" href="#top">
+        <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#e8eaed">
+            <path d="m480-541.85-184 184L253.85-400 480-626.15 706.15-400 664-357.85l-184-184Z" />
+        </svg>
+    </a>
+
+    <header class="container-fluid flex justify-space-between align-items-center flex-wrap grow shrink bg-black-500" id="top">
+        <div class="flex gap-20">
+            <a href="#" class="logo m-1">
+                <svg viewBox="0 0 511.57 491.49">
+                    <path class="fill-white" d="M440,35V317.6c0,2.65-1.05,5.2-2.93,7.07l-92.41,92.4c-1.88,1.88-4.42,2.93-7.07,2.93H205c-8.28,0-15-6.72-15-15v-70c0-8.28,6.72-15,15-15h120c8.28,0,15-6.72,15-15V135c0-8.28-6.72-15-15-15H155c-8.28,0-15,6.72-15,15V405c0,8.28-6.72,15-15,15H55c-8.28,0-15-6.72-15-15V35c0-8.28,6.72-15,15-15H425c8.28,0,15,6.72,15,15Z" />
+                    <rect class="fill-white" x="210" y="190" width="60" height="60" />
+                </svg>
+            </a>
+
+            <div class="nav-bar flex align-items-center color-white font-bold">
+                <a href="#" class="badge">Home</a>
+                <a href="#" class="badge" data-badge="Sale">Sale</a>
+                <a href="#" class="badge" data-badge="New">Products</a>
+                <a href="#" class="badge">Announcement</a>
+            </div>
+        </div>
+
+        <div class="h-100 flex align-items-center gap-10 color-white shrink-0">
+
+            <div class="search-field flex align-items-center gap-5">
+                <input type="search" name="search" class="search-bar p-3 bg-white-500 rounded-md color-black shrink" placeholder="Search Products">
+
+                <button class="search-btn" type="submit" title="search">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000">
+                        <path d="M781.69-136.92 530.46-388.16q-30 24.77-69 38.77-39 14-80.69 14-102.55 0-173.58-71.01-71.03-71.01-71.03-173.54 0-102.52 71.01-173.6 71.01-71.07 173.54-71.07 102.52 0 173.6 71.03 71.07 71.03 71.07 173.58 0 42.85-14.38 81.85-14.39 39-38.39 67.84l251.23 251.23-42.15 42.16ZM380.77-395.38q77.31 0 130.96-53.66 53.66-53.65 53.66-130.96t-53.66-130.96q-53.65-53.66-130.96-53.66t-130.96 53.66Q196.15-657.31 196.15-580t53.66 130.96q53.65 53.66 130.96 53.66Z" />
+                    </svg>
+                </button>
+            </div>
+
+            <a href="#" data-tooltip="Account" class="tooltip">
+                <svg xmlns="http://www.w3.org/2000/svg" height="35px" viewBox="0 -960 960 960" width="35px" fill="#e8eaed">
+                    <path d="M480-492.31q-57.75 0-98.87-41.12Q340-574.56 340-632.31q0-57.75 41.13-98.87 41.12-41.13 98.87-41.13 57.75 0 98.87 41.13Q620-690.06 620-632.31q0 57.75-41.13 98.88-41.12 41.12-98.87 41.12ZM180-187.69v-88.93q0-29.38 15.96-54.42 15.96-25.04 42.66-38.5 59.3-29.07 119.65-43.61 60.35-14.54 121.73-14.54t121.73 14.54q60.35 14.54 119.65 43.61 26.7 13.46 42.66 38.5Q780-306 780-276.62v88.93H180Zm60-60h480v-28.93q0-12.15-7.04-22.5-7.04-10.34-19.11-16.88-51.7-25.46-105.42-38.58Q534.7-367.69 480-367.69q-54.7 0-108.43 13.11-53.72 13.12-105.42 38.58-12.07 6.54-19.11 16.88-7.04 10.35-7.04 22.5v28.93Zm240-304.62q33 0 56.5-23.5t23.5-56.5q0-33-23.5-56.5t-56.5-23.5q-33 0-56.5 23.5t-23.5 56.5q0 33 23.5 56.5t56.5 23.5Zm0-80Zm0 384.62Z" />
+                </svg>
+            </a>
+            <a href="#" data-tooltip="Cart" class="tooltip">
+                <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#e8eaed">
+                    <path d="M252.31-100Q222-100 201-121q-21-21-21-51.31v-455.38Q180-658 201-679q21-21 51.31-21H330v-10q0-62.15 43.92-106.08Q417.85-860 480-860t106.08 43.92Q630-772.15 630-710v10h77.69Q738-700 759-679q21 21 21 51.31v455.38Q780-142 759-121q-21 21-51.31 21H252.31Zm0-60h455.38q4.62 0 8.46-3.85 3.85-3.84 3.85-8.46v-455.38q0-4.62-3.85-8.46-3.84-3.85-8.46-3.85H630v90q0 12.77-8.62 21.38Q612.77-520 600-520t-21.38-8.62Q570-537.23 570-550v-90H390v90q0 12.77-8.62 21.38Q372.77-520 360-520t-21.38-8.62Q330-537.23 330-550v-90h-77.69q-4.62 0-8.46 3.85-3.85 3.84-3.85 8.46v455.38q0 4.62 3.85 8.46 3.84 3.85 8.46 3.85ZM390-700h180v-10q0-37.61-26.19-63.81Q517.62-800 480-800q-37.62 0-63.81 26.19Q390-747.61 390-710v10ZM240-160v-480 480Z" />
+                </svg>
+            </a>
+        </div>
+    </header>
+
+    <div class="hero-section">
+
+    </div>
+
+    <section class="container-fluid first">
+        <div class="section-4-grid grid gap-5">
+            <div class="lists grid align-items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                    <path d="M240-160q-50 0-85-35t-35-85H40v-440q0-33 23.5-56.5T120-800h560v160h120l120 160v200h-80q0 50-35 85t-85 35q-50 0-85-35t-35-85H360q0 50-35 85t-85 35Zm0-80q17 0 28.5-11.5T280-280q0-17-11.5-28.5T240-320q-17 0-28.5 11.5T200-280q0 17 11.5 28.5T240-240ZM120-360h32q17-18 39-29t49-11q27 0 49 11t39 29h272v-360H120v360Zm600 120q17 0 28.5-11.5T760-280q0-17-11.5-28.5T720-320q-17 0-28.5 11.5T680-280q0 17 11.5 28.5T720-240Zm-40-200h170l-90-120h-80v120ZM360-540Z" />
+                </svg>
+                <div class="card p-3 flex flex-column self-start gap-1 grow">
+                    <div class="card-header">
+                        <h4>Shipping</h4>
+                    </div>
+                    <div class="card-body">
+                        <p>Enjoy free and fast shipping on most areas.</p>
+                    </div>
+                    <a href="" class="link">Learn more</a>
+                </div>
+            </div>
+            <div class="lists grid align-items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" style="transform: scaleX(-100%);">
+                    <path d="M40-160v-80h200v-80H80v-80h160v-80H122v-80h118v-118l-78-168 72-34 94 200h464l-78-166 72-34 94 200v520H40Zm440-280h160q17 0 28.5-11.5T680-480q0-17-11.5-28.5T640-520H480q-17 0-28.5 11.5T440-480q0 17 11.5 28.5T480-440ZM320-240h480v-360H320v360Zm0 0v-360 360Z" />
+                </svg>
+                <div class="card p-3 flex flex-column self-start gap-1 grow">
+                    <div class="card-header">
+                        <h4>Return Policy</h4>
+                    </div>
+                    <div class="card-body">
+                        <p>Return your purchase within 30 days for a full refund.</p>
+                    </div>
+                    <a href="" class="link">Learn more</a>
+                </div>
+            </div>
+            <div class="lists grid align-items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                    <path d="M440-82q-76-8-141.5-41.5t-114-87Q136-264 108-333T80-480q0-91 36.5-168T216-780h-96v-80h240v240h-80v-109q-55 44-87.5 108.5T160-480q0 123 80.5 212.5T440-163v81Zm-17-214L254-466l56-56 113 113 227-227 56 57-283 283Zm177 196v-240h80v109q55-45 87.5-109T800-480q0-123-80.5-212.5T520-797v-81q152 15 256 128t104 270q0 91-36.5 168T744-180h96v80H600Z" />
+                </svg>
+                <div class="card p-3 flex flex-column self-start gap-1 grow">
+                    <div class="card-header">
+                        <h4>Replacement Policy</h4>
+                    </div>
+                    <div class="card-body">
+                        <p>Enjoy a 30-day free replacement to all purchase.</p>
+                    </div>
+                    <a href="" class="link">Learn more</a>
+                </div>
+            </div>
+            <div class="lists grid align-items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                    <path d="m438-338 226-226-57-57-169 169-84-84-57 57 141 141Zm42 258q-139-35-229.5-159.5T160-516v-244l320-120 320 120v244q0 152-90.5 276.5T480-80Zm0-84q104-33 172-132t68-220v-189l-240-90-240 90v189q0 121 68 220t172 132Zm0-316Z" />
+                </svg>
+                <div class="card p-3 flex flex-column self-start gap-1 grow">
+                    <div class="card-header">
+                        <h4>1 Year Warranty</h4>
+                    </div>
+                    <div class="card-body">
+                        <p>For all keyboards and Mouse purchased on the site.</p>
+                    </div>
+                    <a href="" class="link">Learn more</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="container-fluid bg-white-200 second">
+        <div class="flex justify-space-between">
+            <div class="flex flex-column flex-wrap gap-1">
+                <h3 class="color-black-500 text-uppercase font-extrabold" style="color: var(--color-primary);">
+                    Need Help?
+                </h3>
+                <p class="m-0 w-33 font-medium text-justify color-black-500">
+                    Need assistance or have questions? Contact our dedicated team for prompt and helpful support. We're here to ensure your satisfaction and provide a seamless experience.
+                </p>
+                <a href="" class="link font-bold" style="width: fit-content; padding: 1rem 0;">Contact Us</a>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
