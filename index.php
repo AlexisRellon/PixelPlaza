05367cb54273b36aa093c19c5e693f76caf1ec9a
AlexisRellon <alexis26rellon@gmail.com>
2024-05-26 20:59:45 +0800

    chore: Update footer layout and add online store links

--- index.php
+++ index.php
@@ -27,10 +27,10 @@
 
 <body>
 
-    <!-- <div class="cookie-banner color-black flex justify-center align-items-center" id="cookie-banner">
+    <div class="cookie-banner color-black flex justify-center align-items-center" id="cookie-banner">
         <p class="m-0">We use cookies to improve your experience on our website. By browsing this website, you agree to our use of cookies.</p>
         <input type="button" class="btn m-3 p-1 bg-black-500 color-white rounded-md" onclick="acceptCookies()" value="Accept">
-    </div> -->
+    </div>
 
     <a type="button" class="to-top absolute rounded-full invisible" href="#top">
         <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#e8eaed">
@@ -190,7 +190,44 @@
     </section>
 
     <section class="container-fluid third">
-        
+        <div class="browse-products grid grid-rows-3 gap-5 align-items-center justify-items-center">
+            <h1 class="m-5 text-center text-uppercase font-extrabold" style="color: var(--color-secondary);">Browse Products</h1>
+
+            <div class="tabs flex gap-20 text-uppercase font-bold align-items-center">
+                <a href="#new" class="link">New Arrivals</a>
+                <a href="#sale" class="link">Best Sellers</a>
+                <a href="#recommended" class="link">Recommended Products</a>
+            </div>
+
+            <div class="products-card grid gap-1">
+
+                <!-- Card template -->
+                <div class="card p-5 flex flex-column justify-start">
+                    <!-- Hover toolbar -->
+                    <div class="hover-toolbar flex justify-center align-items-center self-center">
+                        <button class="button view">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                                <path d="M480-312q70 0 119-49t49-119q0-70-49-119t-119-49q-70 0-119 49t-49 119q0 70 49 119t119 49Zm0-72q-40 0-68-28t-28-68q0-40 28-68t68-28q40 0 68 28t28 68q0 40-28 68t-68 28Zm0 192q-142.6 0-259.8-78.5Q103-349 48-480q55-131 172.2-209.5Q337.4-768 480-768q142.6 0 259.8 78.5Q857-611 912-480q-55 131-172.2 209.5Q622.6-192 480-192Zm0-288Zm0 216q112 0 207-58t146-158q-51-100-146-158t-207-58q-112 0-207 58T127-480q51 100 146 158t207 58Z" />
+                            </svg>
+                        </button>
+                        <button class="button cart">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                                <path d="M444-576v-132H312v-72h132v-132h72v132h132v72H516v132h-72ZM263.79-96Q234-96 213-117.21t-21-51Q192-198 213.21-219t51-21Q294-240 315-218.79t21 51Q336-138 314.79-117t-51 21Zm432 0Q666-96 645-117.21t-21-51Q624-198 645.21-219t51-21Q726-240 747-218.79t21 51Q768-138 746.79-117t-51 21ZM48-792v-72h133l155 360h301l113-264h78L703-476q-9 20-26.5 32T637-432H317l-42 72h493v72H276q-42 0-63-36.5t0-71.5l52-90-131-306H48Z" />
+                            </svg>
+                        </button>
+                    </div>
+                    <img src="../img/assets/No-image-thumbnail.png" alt="" loading="lazy" class="product-image">
+                    <div class="product-details w-100 text-center">
+                        <p class="brand font-bold text-sm">Brand</p>
+                        <p class="product font-extrabold text-2xl">Product</p>
+                        <p class="price text-base">Price</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <a href="#" class="a-btn font-bold" style="width: fit-content;">View All Products</a>
+        </div>
     </section>
 
     <footer class="container-fluid bg-black-500 flex flex-column gap-15 footer">
@@ -214,22 +251,20 @@
         </div>
         <div class="footer-body color-white flex justify-space-between align-items-center">
             <div class="site-map flex gap-50">
-                <div class="company flex flex-column gap-5">
+                <div class="company flex flex-column gap-3">
                     <h6 class="font-bold">Company</h6>
                     <a href="#" class="link">About Us</a>
                     <a href="#" class="link">Contact Us</a>
                     <a href="#" class="link">Careers</a>
                     <a href="#" class="link">Press</a>
                 </div>
-                <div class="products flex flex-column gap-5">
+                <div class="products flex flex-column gap-3">
                     <h6 class="font-bold">Products</h6>
                     <a href="#" class="link">Keyboards</a>
                     <a href="#" class="link">Mice</a>
                     <a href="#" class="link">Headsets</a>
-                    <a href="#" class="link">Mousepads</a>
-                    <a href="#" class="link">Monitors</a>
                 </div>
-                <div class="online-store flex flex-column gap-5">
+                <div class="online-store flex flex-column gap-3">
                     <h6 class="font-bold">Online Stores</h6>
                     <a href="#" class="link">Amazon</a>
                     <a href="#" class="link">eBay</a>
@@ -237,12 +272,10 @@
                     <a href="#" class="link">Walmart</a>
                     <a href="#" class="link">Target</a>
                 </div>
-                <div class="support flex flex-column gap-5">
+                <div class="support flex flex-column gap-3">
                     <h6 class="font-bold">Support</h6>
-                    <a href="#" class="link">FAQ</a>
-                    <a href="#" class="link">Shipping</a>
-                    <a href="#" class="link">Returns</a>
-                    <a href="#" class="link">Warranty</a>
+                    <a href="mailto:moovemberdev@gmail.com" class="link">support@pixelplaza.com</a>
+                    <a href="tel:" class="link">+1 800-123-4567</a>
                 </div>
             </div>
         </div>
