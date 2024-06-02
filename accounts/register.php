ba38d13818beeae163517e961a1341a305453950
AlexisRellon <alexis26rellon@gmail.com>
2024-06-03 02:24:27 +0800

    Added register.php

---
+++ accounts/register.php
@@ -0,0 +1,31 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Account</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Pixel Plaza is a e-commerce platform to buy the best computer peripherals in the market.">
+    <meta name="author" content="Pixel Plaza">
+    <meta name="revisit-after" content="1 day">
+    <meta name="language" content="English">
+
+    <link rel="stylesheet" href="../css/style.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="../css/global-style.css">
+    <link rel="stylesheet" href="../css/utility.min.css">
+
+    <!-- link to google font material icon -->
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+
+    <link rel="shortcut icon" href="../img/brand-logo-white.png" type="image/x-icon">
+    <link rel="icon" href="../img/brand-logo-white.png" type="image/x-icon">
+
+    <script src="../js/script.js"></script>
+
+
+</head>
+
+<body>
