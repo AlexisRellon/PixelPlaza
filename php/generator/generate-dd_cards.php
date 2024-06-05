ef6af8e9122ebb939d09cd25507e531f2afa3490
AlexisRellon <alexis26rellon@gmail.com>
2024-06-05 08:59:19 +0800

    Refactor image source path for product cards

--- php/generator/generate-dd_cards.php
+++ php/generator/generate-dd_cards.php
@@ -3,7 +3,7 @@ function generateCards_dd($product_name, $price, $sale_price)
 {
     return <<<HTML
             <div class="dd-card flex flex-column align-items-center gap-1">
-                <img src="../img/assets/No-image-thumbnail.png" loading="lazy" alt="">
+                <img src="./img/assets/No-image-thumbnail.png" loading="lazy" alt="">
                 <h5>$product_name</h5>
                 <div class="flex gap-1">
                     <del>₱{$price}</del>
