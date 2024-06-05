3879cab256af6a47e364c5213fc070532217f2f1
AlexisRellon <alexis26rellon@gmail.com>
2024-06-05 08:58:33 +0800

    Refactor CSS for online store links and product cards

--- index.php
+++ index.php
@@ -13,17 +13,22 @@
     <meta name="revisit-after" content="1 day">
     <meta name="language" content="English">
 
-    <link rel="stylesheet" href="../css/style.css" type="text/css" media="all" />
-    <link rel="stylesheet" href="../css/global-style.css">
-    <link rel="stylesheet" href="../css/utility.min.css">
+    <!-- Open Graph data -->
+    <meta property="og:title" content="Pixel Plaza" />
+    <meta property="og:type" content="website" />
+
+    <!-- TODO: change to ../css/ before uploading -->
+    <link rel="stylesheet" href="./css/style.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="./css/global-style.css">
+    <link rel="stylesheet" href="./css/utility.min.css">
 
     <!-- link to google font material icon -->
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
 
-    <link rel="shortcut icon" href="../img/brand-logo-white.png" type="image/x-icon">
-    <link rel="icon" href="../img/brand-logo-white.png" type="image/x-icon">
+    <link rel="shortcut icon" href="./img/brand-logo-white.png" type="image/x-icon">
+    <link rel="icon" href="./img/brand-logo-white.png" type="image/x-icon">
 
-    <script src="../js/script.js"></script>
+    <script src="/js/script.js"></script>
 
 
 </head>
@@ -56,7 +61,7 @@
 
     <header class="container-fluid flex justify-space-between align-items-center flex-wrap grow shrink bg-black-500 navigation" id="top">
         <div class="flex gap-20">
-            <a href="#" class="logo m-1">
+            <a href="./index.php" class="logo m-1">
                 <svg viewBox="0 0 511.57 491.49">
                     <path class="fill-white" d="M440,35V317.6c0,2.65-1.05,5.2-2.93,7.07l-92.41,92.4c-1.88,1.88-4.42,2.93-7.07,2.93H205c-8.28,0-15-6.72-15-15v-70c0-8.28,6.72-15,15-15h120c8.28,0,15-6.72,15-15V135c0-8.28-6.72-15-15-15H155c-8.28,0-15,6.72-15,15V405c0,8.28-6.72,15-15,15H55c-8.28,0-15-6.72-15-15V35c0-8.28,6.72-15,15-15H425c8.28,0,15,6.72,15,15Z" />
                     <rect class="fill-white" x="210" y="190" width="60" height="60" />
@@ -64,20 +69,20 @@
             </a>
 
             <div class="nav-bar flex align-items-center color-white font-bold">
-                <a href="#" class="badge">Home</a>
-                <a href="#" class="badge sale" data-badge="Sale">Sale <span class="material-symbols-outlined" id="dropmenu"></span></a>
+                <a href="./index.php" class="badge">Home</a>
+                <a href="./collections/deals.php" class="badge sale" data-badge="Sale">Sale <span class="material-symbols-outlined" id="dropmenu"></span></a>
 
                 <!-- Dropdown -->
-                <div class="dropdown dd-sale p-5 grid color-black">
+                <div class="dropdown dd-sale p-5 color-black">
                     <div class="header">Pixel Plaza Promotions</div>
                     <div class="body grid grid-cols-5">
-                        <a href="#">Sales</a>
+                        <a href="./collections/deals.php">Sales</a>
                         <!-- dropdown card template -->
                         <?php include 'php/generator/generate-dd_cards.php'; ?>
                     </div>
                 </div>
 
-                <a href="#" class="badge new" data-badge="New">Products <span class="material-symbols-outlined" id="dropmenu"></span></a>
+                <a href="./collections/all.php" class="badge new" data-badge="New">Products <span class="material-symbols-outlined" id="dropmenu"></span></a>
 
                 <!-- Dropdown -->
                 <div class="dropdown dd-products p-5">
@@ -87,7 +92,7 @@
                     <a href="/collections/switch.php">Switch</a>
                 </div>
 
-                <a href="#" class="badge">Announcement</a>
+                <a href="./announcement.php" class="badge">Announcement</a>
             </div>
         </div>
 
@@ -103,7 +108,7 @@
                 </button>
             </div>
 
-            <a href="/accounts/login.php" data-tooltip="Account" class="tooltip">
+            <a href="./accounts/login.php" data-tooltip="Account" class="tooltip">
                 <svg xmlns="http://www.w3.org/2000/svg" height="35px" viewBox="0 -960 960 960" width="35px" fill="#e8eaed">
                     <path d="M480-492.31q-57.75 0-98.87-41.12Q340-574.56 340-632.31q0-57.75 41.13-98.87 41.12-41.13 98.87-41.13 57.75 0 98.87 41.13Q620-690.06 620-632.31q0 57.75-41.13 98.88-41.12 41.12-98.87 41.12ZM180-187.69v-88.93q0-29.38 15.96-54.42 15.96-25.04 42.66-38.5 59.3-29.07 119.65-43.61 60.35-14.54 121.73-14.54t121.73 14.54q60.35 14.54 119.65 43.61 26.7 13.46 42.66 38.5Q780-306 780-276.62v88.93H180Zm60-60h480v-28.93q0-12.15-7.04-22.5-7.04-10.34-19.11-16.88-51.7-25.46-105.42-38.58Q534.7-367.69 480-367.69q-54.7 0-108.43 13.11-53.72 13.12-105.42 38.58-12.07 6.54-19.11 16.88-7.04 10.35-7.04 22.5v28.93Zm240-304.62q33 0 56.5-23.5t23.5-56.5q0-33-23.5-56.5t-56.5-23.5q-33 0-56.5 23.5t-23.5 56.5q0 33 23.5 56.5t56.5 23.5Zm0-80Zm0 384.62Z" />
                 </svg>
@@ -250,25 +255,25 @@
         <h1 class="text-uppercase font-bold">Shop by Categories</h1>
 
         <div class="categories relative">
-            <a href="#" class="category item1">
+            <a href="./collections/keyboards.php" class="category item1">
                 <div class="text-container text-capitalize">
                     <h3>keyboards</h3>
                     <p><?php echo $product['quantity'] ?? '0' ?> items</p>
                 </div>
             </a>
-            <a href="#" class="category item2">
+            <a href="./collections/mouse.php" class="category item2">
                 <div class="text-container text-capitalize">
                     <h3>Mouse</h3>
                     <p><?php echo $product['quantity'] ?? '0' ?> items</p>
                 </div>
             </a>
-            <a href="#" class="category item3">
+            <a href="./collections/headsets.php" class="category item3">
                 <div class="text-container text-capitalize">
                     <h3>Headsets</h3>
                     <p><?php echo $product['quantity'] ?? '0' ?> items</p>
                 </div>
             </a>
-            <a href="#" class="category item4">
+            <a href="./collections/switch.php" class="category item4">
                 <div class="text-container text-capitalize">
                     <h3>Switch</h3>
                     <p><?php echo $product['quantity'] ?? '0' ?> items</p>
