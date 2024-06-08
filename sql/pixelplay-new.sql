07b89447d3715c03ba6fbc77cc780e6b21099f0e
AlexisRellon <alexis26rellon@gmail.com>
2024-06-09 01:25:19 +0800

    Change: update on queries

--- sql/pixelplay-new.sql
+++ sql/pixelplay-new.sql
@@ -1,34 +1,39 @@
--- Active: 1714706041615@@127.0.0.1@3306@pixelplay
-
-CREATE TABLE `Products` (
+-- Active: 1714706041615@@127.0.0.1@3306@pixelplaza
+CREATE TABLE IF NOT EXISTS `Products` (
     `ProductID` INT PRIMARY KEY AUTO_INCREMENT, -- Product ID
     `Name` VARCHAR(255) NOT NULL, -- Product Name
     `Description` TEXT, -- Product Description
     `Price` DECIMAL(10, 2) NOT NULL, -- Product Price
+    `SalePrice` DECIMAL(10, 2) DEFAULT 0, -- Sale Price
     `Quantity` INT NOT NULL, -- Product Quantity
-    `ImageURL` VARCHAR(255), -- Product Image URL
+    `ImageURL` TEXT, -- Product Image URL
     `CategoryID` INT, -- Category ID
-    `BrandID` INT, -- Brand ID
+    `BrandID` INT, -- Brand 
+    `ProductStatusID` INT, -- Product Status 
 
-    FOREIGN KEY (`CategoryID`) REFERENCES `Categories` (`CategoryID`) ON DELETE RESTRICT ON UPDATE CASCADE  ,
-    FOREIGN KEY (`BrandID`) REFERENCES `Brands` (`BrandID`) ON DELETE RESTRICT ON UPDATE CASCADE,
     INDEX (`CategoryID`),
-    INDEX (`BrandID`)
+    INDEX (`BrandID`),
+    INDEX (`ProductStatusID`)
 );
 
-CREATE TABLE `Categories` (
+CREATE TABLE IF NOT EXISTS `ProductStatuses` (
+    `ProductStatusID` INT PRIMARY KEY AUTO_INCREMENT, -- Product Status ID
+    `StatusName` VARCHAR(50) NOT NULL -- Status Name e.g. available, out of stock
+);
+
+CREATE TABLE IF NOT EXISTS `Categories` (
     `CategoryID` INT PRIMARY KEY AUTO_INCREMENT, -- Category ID
     `Name` VARCHAR(100) NOT NULL, -- Category Name
     `Description` TEXT -- Category Description
 );
 
-CREATE TABLE `Brands` (
+CREATE TABLE IF NOT EXISTS `Brands` (
     `BrandID` INT PRIMARY KEY AUTO_INCREMENT, -- Brand ID
     `Name` VARCHAR(100) NOT NULL, -- Brand Name
     `Description` TEXT -- Brand Description
 );
 
-CREATE TABLE `Orders` (
+CREATE TABLE IF NOT EXISTS `Orders` (
     `OrderID` INT PRIMARY KEY AUTO_INCREMENT, -- Order ID
     `UserID` INT, -- User ID
     `TotalAmount` DECIMAL(10, 2) NOT NULL, -- Total Amount
@@ -36,21 +41,21 @@ CREATE TABLE `Orders` (
     `CreatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Created At
     `UpdatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- Updated At
 
-    FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`) ON DELETE RESTRICT ON UPDATE CASCADE
+    INDEX (`UserID`)
 );
 
-CREATE TABLE `OrderItems` (
+CREATE TABLE IF NOT EXISTS `OrderItems` (
     `OrderItemID` INT PRIMARY KEY AUTO_INCREMENT, -- Order Item ID
     `OrderID` INT, -- Order ID
     `ProductID` INT, -- Product ID
     `Quantity` INT NOT NULL, -- Quantity
     `Price` DECIMAL(10, 2) NOT NULL, -- Price
 
-    FOREIGN KEY (`OrderID`) REFERENCES `Orders` (`OrderID`) ON DELETE RESTRICT ON UPDATE CASCADE,
-    FOREIGN KEY (`ProductID`) REFERENCES `Products` (`ProductID`) ON DELETE RESTRICT ON UPDATE CASCADE
+    INDEX (`OrderID`),
+    INDEX (`ProductID`)
 );
 
-CREATE TABLE `Reviews` (
+CREATE TABLE IF NOT EXISTS `Reviews` (
     `ReviewID` INT PRIMARY KEY AUTO_INCREMENT, -- Review ID
     `ProductID` INT, -- Product ID
     `UserID` INT, -- User ID
@@ -58,31 +63,43 @@ CREATE TABLE `Reviews` (
     `Comment` TEXT, -- Comment
     `CreatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Created At
 
-    FOREIGN KEY (`ProductID`) REFERENCES `Products` (`ProductID`) ON DELETE RESTRICT ON UPDATE CASCADE,
-    FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`) ON DELETE RESTRICT ON UPDATE CASCADE
+    INDEX (`ProductID`),
+    INDEX (`UserID`)
 );
 
-CREATE TABLE `Users` (
+CREATE TABLE IF NOT EXISTS `Users` (
     `UserID` INT PRIMARY KEY AUTO_INCREMENT, -- User ID
-    `Username` VARCHAR(100) NOT NULL, -- Username
-    `Password` VARCHAR(255) NOT NULL, -- Password
-    `Email` VARCHAR(255) NOT NULL, -- Email
+    `Username` VARCHAR(100) NOT NULL UNIQUE, -- Username
+    `Password` VARCHAR(255) NOT NULL, -- Password | hashed
+    `Email` VARCHAR(255) NOT NULL UNIQUE, -- Email
     `FirstName` VARCHAR(100), -- First Name
     `LastName` VARCHAR(100), -- Last Name
-    `Address` TEXT, -- Address
-    `Phone` VARCHAR(20) -- Phone
+    `AddressID` TEXT, -- Address
+    `Phone` VARCHAR(20), -- Phone
+    `UserTypeID` INT, -- User Type ID
+    `attempts` INT NOT NULL DEFAULT 0, -- Login Attempts
+    `UserStatusID` INT, -- Status ID
+
+    INDEX (`AddressID`),
+    INDEX (`UserTypeID`),
+    INDEX (`UserStatusID`)
 );
 
-CREATE TABLE `UserTypes` (
+CREATE TABLE IF NOT EXISTS `Addresses` (
+    `AddressID` INT PRIMARY KEY AUTO_INCREMENT, -- Address ID
+    `Address` TEXT, -- Address
+    `City` VARCHAR(100), -- City
+    `State` VARCHAR(100), -- State
+    `Country` VARCHAR(100), -- Country
+    `ZipCode` VARCHAR(20) -- Zip Code
+);
+
+CREATE TABLE IF NOT EXISTS `UserStatus` (
+    `UserStatusID` INT PRIMARY KEY AUTO_INCREMENT, -- Status ID
+    `StatusName` VARCHAR(50) NOT NULL -- Status Name e.g. verified, pending, blocked
+);
+
+CREATE TABLE IF NOT EXISTS `UserTypes` (
     `UserTypeID` INT PRIMARY KEY AUTO_INCREMENT, -- User Type ID
     `TypeName` VARCHAR(50) NOT NULL -- Type Name e.g. Admin, Customer
-);
-
-CREATE TABLE `UserRoles` (
-    `UserRoleID` INT PRIMARY KEY AUTO_INCREMENT, -- User Role ID
-    `UserID` INT, -- User ID :FK to Users 
-    `RoleID` INT, -- Role ID :FK to UserTypes
-
-    FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`) ON DELETE RESTRICT ON UPDATE CASCADE,
-    FOREIGN KEY (`RoleID`) REFERENCES `UserTypes` (`UserTypeID`) ON DELETE RESTRICT ON UPDATE CASCADE
 );
---
+++ sql/pixelplaza.sql
@@ -0,0 +1,374 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 07, 2024 at 06:16 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pixelplaza`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `addresses`
+--
+
+CREATE TABLE `addresses` (
+  `AddressID` int(11) NOT NULL,
+  `Address` text DEFAULT NULL,
+  `City` varchar(100) DEFAULT NULL,
+  `State` varchar(100) DEFAULT NULL,
+  `Country` varchar(100) DEFAULT NULL,
+  `ZipCode` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+
+CREATE TABLE `brands` (
+  `BrandID` int(11) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `Description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `CategoryID` int(11) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `Description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderitems`
+--
+
+CREATE TABLE `orderitems` (
+  `OrderItemID` int(11) NOT NULL,
+  `OrderID` int(11) DEFAULT NULL,
+  `ProductID` int(11) DEFAULT NULL,
+  `Quantity` int(11) NOT NULL,
+  `Price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `OrderID` int(11) NOT NULL,
+  `UserID` int(11) DEFAULT NULL,
+  `TotalAmount` decimal(10,2) NOT NULL,
+  `Status` varchar(50) DEFAULT NULL,
+  `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `UpdatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `ProductID` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Description` text DEFAULT NULL,
+  `Price` decimal(10,2) NOT NULL,
+  `SalePrice` decimal(10,2) DEFAULT 0.00,
+  `Quantity` int(11) NOT NULL,
+  `ImageURL` text DEFAULT NULL,
+  `CategoryID` int(11) DEFAULT NULL,
+  `BrandID` int(11) DEFAULT NULL,
+  `ProductStatusID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productstatuses`
+--
+
+CREATE TABLE `productstatuses` (
+  `ProductStatusID` int(11) NOT NULL,
+  `StatusName` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `ReviewID` int(11) NOT NULL,
+  `ProductID` int(11) DEFAULT NULL,
+  `UserID` int(11) DEFAULT NULL,
+  `Rating` int(11) NOT NULL,
+  `Comment` text DEFAULT NULL,
+  `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `UserID` int(11) NOT NULL,
+  `Username` varchar(100) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `FirstName` varchar(100) DEFAULT NULL,
+  `LastName` varchar(100) DEFAULT NULL,
+  `AddressID` int(11) DEFAULT NULL,
+  `Phone` int(11) DEFAULT NULL,
+  `UserTypeID` int(11) DEFAULT NULL,
+  `attempts` int(11) NOT NULL DEFAULT 0,
+  `UserStatusID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userstatus`
+--
+
+CREATE TABLE `userstatus` (
+  `UserStatusID` int(11) NOT NULL,
+  `StatusName` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usertypes`
+--
+
+CREATE TABLE `usertypes` (
+  `UserTypeID` int(11) NOT NULL,
+  `TypeName` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `addresses`
+--
+ALTER TABLE `addresses`
+  ADD PRIMARY KEY (`AddressID`);
+
+--
+-- Indexes for table `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`BrandID`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`CategoryID`);
+
+--
+-- Indexes for table `orderitems`
+--
+ALTER TABLE `orderitems`
+  ADD PRIMARY KEY (`OrderItemID`),
+  ADD KEY `OrderID` (`OrderID`),
+  ADD KEY `ProductID` (`ProductID`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`OrderID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`ProductID`),
+  ADD KEY `CategoryID` (`CategoryID`),
+  ADD KEY `BrandID` (`BrandID`),
+  ADD KEY `ProductStatusID` (`ProductStatusID`);
+
+--
+-- Indexes for table `productstatuses`
+--
+ALTER TABLE `productstatuses`
+  ADD PRIMARY KEY (`ProductStatusID`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`ReviewID`),
+  ADD KEY `ProductID` (`ProductID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UserID`),
+  ADD UNIQUE KEY `Username` (`Username`),
+  ADD UNIQUE KEY `Email` (`Email`),
+  ADD KEY `AddressID` (`AddressID`),
+  ADD KEY `UserTypeID` (`UserTypeID`),
+  ADD KEY `UserStatusID` (`UserStatusID`);
+
+--
+-- Indexes for table `userstatus`
+--
+ALTER TABLE `userstatus`
+  ADD PRIMARY KEY (`UserStatusID`);
+
+--
+-- Indexes for table `usertypes`
+--
+ALTER TABLE `usertypes`
+  ADD PRIMARY KEY (`UserTypeID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `addresses`
+--
+ALTER TABLE `addresses`
+  MODIFY `AddressID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `BrandID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `CategoryID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orderitems`
+--
+ALTER TABLE `orderitems`
+  MODIFY `OrderItemID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `OrderID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `productstatuses`
+--
+ALTER TABLE `productstatuses`
+  MODIFY `ProductStatusID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `ReviewID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `userstatus`
+--
+ALTER TABLE `userstatus`
+  MODIFY `UserStatusID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `usertypes`
+--
+ALTER TABLE `usertypes`
+  MODIFY `UserTypeID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orderitems`
+--
+ALTER TABLE `orderitems`
+  ADD CONSTRAINT `orderitems_ibfk_1` FOREIGN KEY (`OrderID`) REFERENCES `orders` (`OrderID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `orderitems_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`ProductStatusID`) REFERENCES `productstatuses` (`ProductStatusID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`CategoryID`) REFERENCES `categories` (`CategoryID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `products_ibfk_3` FOREIGN KEY (`BrandID`) REFERENCES `brands` (`BrandID`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`UserTypeID`) REFERENCES `usertypes` (`UserTypeID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`UserStatusID`) REFERENCES `userstatus` (`UserStatusID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `users_ibfk_3` FOREIGN KEY (`AddressID`) REFERENCES `addresses` (`AddressID`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
