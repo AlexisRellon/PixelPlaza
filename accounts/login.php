d5b3b1028a4c56fdce28e00f75e5f1a92460fcea
AlexisRellon <alexis26rellon@gmail.com>
2024-06-11 06:37:21 +0800

    refactor: update login page to store and display user address information

--- accounts/login.php
+++ accounts/login.php
@@ -104,7 +104,7 @@ ob_start();
                 </button>
             </div>
 
-            <a href="<?php echo $url ?? '../accounts/login.php' ?>" data-tooltip="Account" class="tooltip">
+            <a href="../accounts/login.php" data-tooltip="Account" class="tooltip">
                 <svg xmlns="http://www.w3.org/2000/svg" height="35px" viewBox="0 -960 960 960" width="35px" fill="#e8eaed">
                     <path d="M480-492.31q-57.75 0-98.87-41.12Q340-574.56 340-632.31q0-57.75 41.13-98.87 41.12-41.13 98.87-41.13 57.75 0 98.87 41.13Q620-690.06 620-632.31q0 57.75-41.13 98.88-41.12 41.12-98.87 41.12ZM180-187.69v-88.93q0-29.38 15.96-54.42 15.96-25.04 42.66-38.5 59.3-29.07 119.65-43.61 60.35-14.54 121.73-14.54t121.73 14.54q60.35 14.54 119.65 43.61 26.7 13.46 42.66 38.5Q780-306 780-276.62v88.93H180Zm60-60h480v-28.93q0-12.15-7.04-22.5-7.04-10.34-19.11-16.88-51.7-25.46-105.42-38.58Q534.7-367.69 480-367.69q-54.7 0-108.43 13.11-53.72 13.12-105.42 38.58-12.07 6.54-19.11 16.88-7.04 10.35-7.04 22.5v28.93Zm240-304.62q33 0 56.5-23.5t23.5-56.5q0-33-23.5-56.5t-56.5-23.5q-33 0-56.5 23.5t-23.5 56.5q0 33 23.5 56.5t56.5 23.5Zm0-80Zm0 384.62Z" />
                 </svg>
@@ -218,22 +218,26 @@ ob_start();
         if ($result->num_rows > 0) {
             $row = $result->fetch_assoc();
             if (password_verify($password, $row['Password'])) {
-                $_SESSION['Email'] = $row['Email'];
-                $_SESSION['FirstName'] = $row['FirstName'];
-                $_SESSION['LastName'] = $row['LastName'];
                 $_SESSION['UID'] = $row['UserID'];
                 $_SESSION['Role'] = $row['UserTypeID'];
-                $AddressID = $row['AddressID'];
+                $_SESSION['AddressID'] = $row['AddressID'];
 
-                $sql = "SELECT Country FROM `address_table` WHERE AddressID = '$AddressID'";
-                $result = $conn->query($sql);
+                $_SESSION['FirstName'] = $row['FirstName'];
+                $_SESSION['LastName'] = $row['LastName'];
+                $_SESSION['Email'] = $row['Email'];
+                $_SESSION['Phone'] = $row['Phone'];
 
-                if ($result->num_rows > 0) {
-                    $row = $result->fetch_assoc();
-                    $_SESSION['Country'] = $row['Country'];
-                }
+                $sql2 = "SELECT * FROM address_table WHERE AddressID = " . $row['AddressID'];
+                $result2 = $conn->query($sql2);
+                $row2 = $result2->fetch_assoc();
 
-                $_SESSION['Country'] = '';
+                $_SESSION['Address'] = $row2['Address'];
+                $_SESSION['City'] = $row2['City'];
+                $_SESSION['State'] = $row2['State'];
+                $_SESSION['ZipCode'] = $row2['Zipcode'];
+                $_SESSION['Country'] = $row2['Country'];
+
+
                 header('Location: ../index.php');
             } else {
                 echo <<<HTML
