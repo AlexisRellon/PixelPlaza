e2bfa1757ec60c9a1bbc17fd5713ac02c9108f12
AlexisRellon <alexis26rellon@gmail.com>
2024-06-14 21:36:25 +0800

    refactor: Add initial structure for managing orders in admin panel

---
+++ Admin/manage-orders.php
@@ -0,0 +1,12 @@
+<style>
+    @import url('../css/dashboard-card-style.css');
+</style>
+
+<body>
+    <header style="margin-bottom: 2rem;">
+        <h2>Manage Orders</h2>
+    </header>
+    <main>
+        
+    </main>
+</body>
